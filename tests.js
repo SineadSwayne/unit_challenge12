b050b5333f98d788af985573ef2851d5dbc7e3bc
SineadSwayne <ssinead3@gmail.com>
2017-03-03 09:07:28 -0800

    Modifications from help of Temple

--- tests.js
+++ tests.js
@@ -1,15 +1,28 @@
 QUnit.test("Test the getAreaCode function.", function (assert) {
-    var num = "(415) 555-5555";
+    var num = "(415) 234-5678";
     var result = getAreaCode(num);
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
 
 QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
     assert.throws( function() {
-        getAreCode("415)444-5555");
+        getAreCode("(415) 234-5678");
     }, "Missing '('. An error should have been thrown." );
 
     assert.throws( function() {
         getAreCode("(415 444-5555");
     }, "Missing ')'. An error should have been thrown." );
+});
+
+
+QUnit.test("Test the getCoCode function.", function (assert) {
+    var num = "(415) 234-5678";
+    var result = getAreaCode(num);
+    assert.deepEqual(result, "234", "Valid co code test passed.");
+});
+
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 234-5678";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "5678", "Valid line code test passed.");
 });
