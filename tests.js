e21a29e451d6eba2bd10a866cf6c31f9ce6c4339
SineadSwayne <ssinead3@gmail.com>
2017-03-07 08:10:38 -0800

    Tests modification

--- tests.js
+++ tests.js
@@ -4,18 +4,18 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
 
-QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
+QUnit.test( "Test the getLineCode", function( assert ) {
     assert.throws( function() {
         getAreCode("(415) 234-5678");
     }, "Missing '('. An error should have been thrown." );
 
-    assert.throws( function() {
-        getAreCode("(415 444-5555");
+    assert.throws(Test the CoCodefunction() {
+        getAreCode("(415) 234-5678");
     }, "Missing ')'. An error should have been thrown." );
 });
 
 
-QUnit.test("Test the getCoCode function.", function (assert) {
+QUnit.test("Test the getAreaCode"Test the getCoCode function.", function (assert) {
     var num = "(415) 234-5678";
     var result = getAreaCode(num);
     assert.deepEqual(result, "234", "Valid co code test passed.");
